eab422c3bca46f1be886b1d7581e363e356e8053
Kumar Sambhav Jain <kumar.sambhav.jain@gmail.com>
2021-01-30 11:20:39 +0530

    Gradle version upgrade

--- sparkling/build.gradle.kts
+++ sparkling/build.gradle.kts
@@ -2,10 +2,20 @@ plugins {
     scala
     application
 }
+repositories {
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
+}
 
 dependencies {
-    implementation "org.apache.spark:spark-sql_2.12:3.1.0"
-    implementation("org.scala-lang:scala-library:2.13.4")
-
-    testImplementation("org.scalatest:scalatest_2.13:3.2.3")
+    implementation("org.apache.spark:spark-sql_2.12:3.1.0")
+    implementation("org.scala-lang:scala-library:2.12.11")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    testImplementation("org.apache.spark:spark-sql_2.12:3.1.0:")
+    testImplementation("org.scalatest:scalatest_2.12:3.2.3")
 }
---
+++ sparkling/src/test/scala/com/samsoft/SparkTestDemo.scala
@@ -0,0 +1,12 @@
+package com.samsoft
+
+import org.scalatest.funsuite.AnyFunSuite
+
+
+class SparkTestDemo extends AnyFunSuite {
+
+  test("test") {
+    println("test")
+  }
+
+}
